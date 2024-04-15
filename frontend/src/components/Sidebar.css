6cc0f7465c78ec9b97f545823f5d9fee9983a8da
Saisrihitha.M <139038259+Saisrhitha@users.noreply.github.com>
2024-04-15 16:21:09 +0530

    2nd commit

---
+++ frontend/src/components/Sidebar.css
@@ -0,0 +1,112 @@
+/* Sidebar.css */
+.sidebar {
+  width: 170px;
+  background-color: #030e2d;
+  height: calc(100vh - 50px);
+  /* Adjust height minus navbar height */
+  position: fixed;
+  top: 97px;
+  /* Set top position to navbar height */
+  left: 0;
+  z-index: 1000;
+  /* Ensure sidebar is above other content */
+}
+
+.sidebar ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.sidebar li {
+  padding: 10px;
+}
+
+/* .sidebar li a {
+  text-decoration: none;
+  color: #ffffff;
+} */
+/* ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+} */
+
+li a,
+.dropbtn {
+  display: inline-block;
+  color: rgb(252, 249, 249);
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover,
+.dropdown:hover .dropbtn {
+  background-color: rgb(2, 2, 2);
+  
+}
+
+li.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #000000;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: rgb(255, 255, 255);
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #312a2a;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+  width: 10px;
+}
+
+
+/* styles.css
+.sidebar {
+  width: 200px;
+  background-color: #e64c4c;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.sidebar ul {
+  list-style-type: none;
+  padding: 50;
+}
+
+.sidebar li {
+  padding: 10px;
+}
+
+.sidebar li a {
+  text-decoration: none;
+  color: #1b0e0e;
+}
+
+.home {
+  margin-left: 200px;
+  /* Adjust margin to accommodate the sidebar width */
---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,61 @@
+// Sidebar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./css/Sidebar.css";
+
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>
+                    <a href="/Home">Home</a>
+                </li>
+                {/* <li><a href="#news">News</a></li> */}
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">OS</a>
+                    <div class="dropdown-content">
+                        <a href="#">UNIT 1</a>
+                        <a href="#">UNIT 2</a>
+                        <a href="#">UNIT 3</a>
+                    </div>
+                </li>
+                <li className="nav-item pe-3 ">
+                     <a href='/UploadVideo'>
+                        <button className="btn btn-dark font-monospace">Upload Video</button>
+                    </a>
+                        
+                   
+
+                </li>
+                <li className="nav-item pe-3 ">
+                    <Link to='/UploadPDF'>
+                        <button className="btn btn-dark font-monospace">Upload PDF</button>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
+
+
+// import React from "react";
+// import "./Sidebar.css";
+
+
+
+
+// const Sidebar = () => { 
+//     return ( 
+//         <div className="sidebar">
+//             <a href="/home" >Home</a>
+            
+//         </div>
+
+           
+//      );
+// }
+ 
+// export default Sidebar;
